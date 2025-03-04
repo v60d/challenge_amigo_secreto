959d47fab0dc5d69a4253c20c0dd666d5d22edc1
Jovhany Contreras Pascual <130109268+v60d@users.noreply.github.com>
2025-03-04 18:01:15 +0000

    actualizarLista, contadorNimbres

--- app.js
+++ app.js
@@ -2,29 +2,51 @@
 
 // Array para almacenar los nombres
 let listaAmigos = [];
-     let nombresMostrados = [];
+let nombresMostrados = [];
 
-     //Función para manejar la tecla enter
+//Función para manejar la tecla enter
      function manejarEnter(event){
           if(event.key === 'Enter'){
                agregarAmigo();
           }
      }
 
-     //Función para agregar un nombre al array
+//Función para agregar un nombre al array
      function agregarAmigo(){
           const input = document.getElementById('amigo');
           const nombre = input.value.trim();
 
           if(nombre) {
+
                if (listaAmigos.includes(nombre)) {
                     alert('Este nombre ya está en la lista');
                     return;
                }
-               listaAmigos.push(nombre);
-               input.value = '';
-               actualizarListaAmigos();
+
+          listaAmigos.push(nombre);
+          input.value = '';
+          actualizarListaAmigos();
           } else {
                alert('Debes ingresar un nombre');
           }
+     }
+
+//Función para actualizar la lista de amigos en la página
+
+     function actualizarListaAmigos(){
+          const lista = document.getElementById('lista-amigos');
+          lista.innerHTML = '';
+
+          listaAmigos.forEach((nombre, index) => {
+          const item = document.createElement('li');
+          item.textContent = nombre;
+          if (index === listaAmigos.length - 1) {
+               item.classList.add('resaltado');
+          }
+          lista.appendChild(item);
+          });
+
+//Actualizar el contador de nombres mostrados
+          const contador = document.getElementById('contadorNombres');
+          contador.textContent = `Nombres agregados: ${listaAmigos.length}`;
      }
